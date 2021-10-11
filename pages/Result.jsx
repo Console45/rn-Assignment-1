871f2c87b898bfd09d26cc24fee747fcc7e4c06e
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-10-11 17:01:36 +0000

    update

--- pages/Result.jsx
+++ pages/Result.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, Button } from "react-native";
 import { FormOutput } from "../components";
+import { useStore } from "../hooks";
 
 export const Result = ({ navigation }) => {
+  const setFormData = useStore((state) => state.setFormData);
+  function goBack() {
+    setFormData(undefined);
+    navigation.goBack();
+  }
   return (
     <View>
       <FormOutput />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Go back" onPress={goBack} />
     </View>
   );
 };
